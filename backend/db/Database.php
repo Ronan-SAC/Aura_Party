5abd219f3d058b5028223f54091e8fb6c5fd40d5
Ronan <ronanscampos@hotmail.com>
2025-01-17 15:00:34 -0400

    Create_Database

---
+++ backend/db/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database{
+    private $server = "localhost";
+    private $dbnome = "Aura_party";
+    private $user = "root";
+    private $password = "";
+
+    public function connect(){
+        try {
+            $conn = new PDO(
+                "mysql:host=".$this->server.";"."dbname=" .$this->dbnome.";", $this->user, $this->password
+            );
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        }
+    
+
+        catch (\Throwable $th) {
+            echo "Erro". $th->getMessage();
+        }
+    }
+}
