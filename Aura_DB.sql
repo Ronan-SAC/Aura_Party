2e82f45b22975faea39a38eedc6360c4a7afb28b
Ronan <ronanscampos@hotmail.com>
2025-01-21 14:07:10 -0400

    Fixing_Aura_DB.sql

--- Aura_DB.sql
+++ Aura_DB.sql
@@ -25,6 +25,8 @@ cpf char(11),
 email varchar(100)
 );
 
+
+
 insert into Cliente values(
 
 );
@@ -36,20 +38,28 @@ senhaAdm varchar(30)
 
 );
 
-insert into Adm values(
-
+insert into Adm (loginUser,senhaAdm)values(
+'admin','123'
 );
+
+drop table Espacos;
 
 create table Espacos(
 idEspaco int primary key auto_increment,
+idUser int,
 nomeEspaco varchar(50),
 enderecoEspaco varchar (80) not null,
 tipo enum ("Casa", "Salão de festas", "Apartamento","Club") ,
 descricaoEspaco varchar(500),
-lotacaoMax int not null
+lotacaoMax int not null,
+date_Espaco date,
+horario_inicio time,
+horario_fim time,
+foreign key(idUser) references Cliente(idUser)
 );
 
-insert into Espacos values (
-
+insert into Espacos (nomeEspaco,enderecoEspaco,tipo,descricaoEspaco,lotacaoMax,date_Espaco,horario_inicio,horario_fim) values (
+'Casa do João', 'rua do joão', 'Casa','a casa do joao muito legal','10','2025-01-30','17:00','21:00'
 );
 
+select * from Espacos;
